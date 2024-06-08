5a17c8ef762e8fc41178513eccd23fce48b62cdd
AnasMughal15 <108098355+AnasMughal15@users.noreply.github.com>
2024-06-08 17:20:41 +0500

    initial work done

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBtsiUmZhZLWO1KfSeJ9ERRm2t6CpBvSqw",
+  authDomain: "iphone-shop-d3cea.firebaseapp.com",
+  projectId: "iphone-shop-d3cea",
+  storageBucket: "iphone-shop-d3cea.appspot.com",
+  messagingSenderId: "573893097905",
+  appId: "1:573893097905:web:301c70bf29d89fcba06590",
+  measurementId: "G-Z5J72N6LC6"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
